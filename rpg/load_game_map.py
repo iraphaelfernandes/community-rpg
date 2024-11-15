1d26b02bc53a24d2e7bcd96ae57b72d94dfcade6
Raphael Fernandes <raphael.fernandes.eng@gmail.com>
2024-11-15 21:05:59 +0100

    Load game map improved

--- rpg/load_game_map.py
+++ rpg/load_game_map.py
@@ -1,6 +1,3 @@
-"""
-Load maps
-"""
 import json
 import os
 from collections import OrderedDict
@@ -29,27 +26,16 @@ def load_map(map_name):
     """
     Load a map
     """
-
     game_map = GameMap()
     game_map.map_layers = OrderedDict()
-
     game_map.light_layer = LightLayer(100, 100)
 
     # List of blocking sprites
-
     layer_options = {
-        "trees_blocking": {
-            "use_spatial_hash": True,
-        },
-        "misc_blocking": {
-            "use_spatial_hash": True,
-        },
-        "bridges": {
-            "use_spatial_hash": True,
-        },
-        "water_blocking": {
-            "use_spatial_hash": True,
-        },
+        "trees_blocking": {"use_spatial_hash": True},
+        "misc_blocking": {"use_spatial_hash": True},
+        "bridges": {"use_spatial_hash": True},
+        "water_blocking": {"use_spatial_hash": True},
     }
 
     # Read in the tiled map
@@ -95,7 +81,7 @@ def load_map(map_name):
                     )
                 character_sprite.position = shape
             elif isinstance(shape, list) and len(shape[0]) == 2:
-                # Rect or polygon.
+                # Rect or polygon
                 location = [shape[0][0], shape[0][1]]
                 character_sprite = PathFollowingSprite(
                     f":characters:{character_data['images']}"
@@ -127,10 +113,7 @@ def load_map(map_name):
 
             if isinstance(shape, list) and len(shape) == 2:
                 # Point
-                if "radius" in light_object.properties:
-                    radius = light_object.properties["radius"]
-                else:
-                    radius = 150
+                radius = light_object.properties.get("radius", 150)
                 mode = "soft"
                 color = light_object.properties["color"]
                 color = (color.red, color.green, color.blue)
@@ -140,17 +123,12 @@ def load_map(map_name):
             else:
                 print("Failed to add light")
     else:
-        # Hack
-        x = 0
-        y = 0
-        radius = 1
-        mode = "soft"
-        color = arcade.csscolor.WHITE
+        # Hack: Add default light if no lights are found
+        x, y, radius, mode, color = 0, 0, 1, "soft", arcade.csscolor.WHITE
         dummy_light = Light(x, y, radius, color, mode)
         game_map.light_layer.add(dummy_light)
         print("Added default light")
 
-    # Get all the tiled sprite lists
     # Get all the tiled sprite lists
     game_map.map_layers = my_map.sprite_lists
 
@@ -162,12 +140,11 @@ def load_map(map_name):
 
     game_map.properties = my_map.properties
 
-    # Any layer with '_blocking' in it, will be a wall
+    # Add wall list for blocking layers
     game_map.scene.add_sprite_list("wall_list", use_spatial_hash=True)
     for layer, sprite_list in game_map.map_layers.items():
         if "_blocking" in layer:
             game_map.scene.remove_sprite_list_by_object(sprite_list)
-
             game_map.scene["wall_list"].extend(sprite_list)
 
     return game_map
@@ -178,35 +155,10 @@ def load_maps():
     Load all the Tiled maps from a directory.
     (Must use the .json extension.)
     """
-
-    # Directory to pull maps from
     mypath = "resources/maps"
 
     if load_maps.map_file_names is None:
-
         # Dictionary to hold all our maps
         load_maps.map_list = {}
 
-        # Pull names of all json files in that path
-        load_maps.map_file_names = [
-            f[:-5]
-            for f in os.listdir(mypath)
-            if isfile(join(mypath, f)) and f.endswith(".json")
-        ]
-        load_maps.map_file_names.sort()
-        load_maps.file_count = len(load_maps.map_file_names)
-
-    # Loop and load each file
-    map_name = load_maps.map_file_names.pop(0)
-    load_maps.map_list[map_name] = load_map(f"resources/maps/{map_name}.json")
-
-    files_left = load_maps.file_count - len(load_maps.map_file_names)
-    progress = 100 * files_left / load_maps.file_count
-
-    done = len(load_maps.map_file_names) == 0
-    return done, progress, load_maps.map_list
-
-
-load_maps.map_file_names = None
-load_maps.map_list = None
-load_maps.file_count = None
+        # Pull names of
